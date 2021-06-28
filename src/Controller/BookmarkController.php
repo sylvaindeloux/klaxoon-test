38e81985c6366667326eaca9a18ac0c322613fb8
Sylvain Deloux <sylvain.deloux@quitoque.fr>
2021-06-28 19:37:35 +0200

    Cleanup

--- src/Controller/BookmarkController.php
+++ src/Controller/BookmarkController.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\Bookmark;
+use App\Entity\Tag;
 use App\Repository\BookmarkRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,12 +18,19 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 final class BookmarkController extends AbstractController
 {
+    private BookmarkRepository $bookmarkRepository;
+
+    public function __construct(BookmarkRepository $bookmarkRepository)
+    {
+        $this->bookmarkRepository = $bookmarkRepository;
+    }
+
     /**
      * @Route(path = "/{id}", methods = {"DELETE"})
      */
-    public function delete(Bookmark $bookmark, BookmarkRepository $bookmarkRepository): JsonResponse
+    public function delete(Bookmark $bookmark): JsonResponse
     {
-        $bookmarkRepository->delete($bookmark);
+        $this->bookmarkRepository->delete($bookmark);
 
         return new JsonResponse([], Response::HTTP_NO_CONTENT);
     }
@@ -31,14 +40,14 @@ final class BookmarkController extends AbstractController
      *
      * @ParamConverter(name = "tag", class = Tag::class, options = {"id" = "tag_id"})
      */
-    public function removeTag(Bookmark $bookmark, Tag $tag, BookmarkRepository $bookmarkRepository): JsonResponse
+    public function removeTag(Bookmark $bookmark, Tag $tag): JsonResponse
     {
         if (!$bookmark->getTags()->contains($tag)) {
             // silent error
             return new JsonResponse([], Response::HTTP_NO_CONTENT);
         }
 
-        $bookmarkRepository->removeTag($bookmark, $tag);
+        $this->bookmarkRepository->removeTag($bookmark, $tag);
 
         return new JsonResponse([], Response::HTTP_NO_CONTENT);
     }
